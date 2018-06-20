21b10107c57b7837cbe93cbe3b44785a71195ca6
Patrick Folch <patrickfolch2@gmail.com>
2018-06-20 17:45:28 +0200

    home creado

---
+++ app/components/home/home/home.component.js
@@ -0,0 +1,24 @@
+export const HomeComponent ={
+    template:`
+<div class="container-fluid">
+    <div class="row" >
+        <div class="col home" >
+            <h1>HOME</h1>
+            
+            <h3><strong>No hay ninguna pelicula o serie solo imagens y su dexcipcion!!!!!</strong></h3>
+
+            
+            <p>Esta aplicacion recoge las peliculas y series mas vistas<br>
+                de una pagina llamada peliculasDB por medio de la api de <br>
+                usuario de la pagina antes mencionada.</br>
+                Se ha utilizado angular para la funcionalidad: babel y browserify ,para</br>
+                minificar, para arrancar el servidor dela pagina se ha utilizado: express,</br>
+                concurrently,nodemon,wachify y parallelshel.<br>
+
+                visita mi pagina en <a href="https://github.com/PatrickFolch/PatrickFolch.io" target="_blank"><strong>GitHub</strong></a>
+            </p>
+        </div>
+    </div>
+</div>
+`
+}
--- app/components/home/index.js
+++ app/components/home/index.js
@@ -7,16 +7,22 @@ import {SeriesListComponent}
 from "./series/series-list/series-list.component";
 import{FilmsItemComponent} from "./films/films-item/films-item.component";
 import{SeriesItemComponent} from "./series/series-item/series-item.component";
+import{HomeComponent} from "./home/home.component"
 const home = angular
 .module('home',[])
 .service('FilmsService', FilmsService)
 .service('SeriesService', SeriesService)    
+.component('homeIndex',HomeComponent)
 .component ('filmsList', FilmsListComponent)
 .component ('seriesList', SeriesListComponent)
 .component ('filmsItem', FilmsItemComponent)
 .component ('seriesItem', SeriesItemComponent)
 .config(($stateProvider, $urlRouterProvider)=>{
     $stateProvider
+        .state('home', {
+            url: '/',
+            component: 'homeIndex'
+        })
         .state('films', {
             url: '/films',
             component: 'filmsList'
